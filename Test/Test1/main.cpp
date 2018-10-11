be86901c70c363a1a40da725e53a1d844f2853f3
LZX <lzx1453536003@163.com>
2018-10-11 21:09:20 +0800

    Test  main.cpp

---
+++ Test/Test1/main.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+
+void main()
+{
+	int datas[] = { -4, 5, 10, -8, 9, -10, 5, 4, 9, -10, 9, 5, 8 };
+
+}
+
+
+
+
+void Caculate(const int resDats[], const int resLen, int * desData, int * desLen)
+{
+
+}
